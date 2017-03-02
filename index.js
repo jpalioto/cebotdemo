cb0c1164699dd90f8640494a825251b12eff5ad2
John Alioto <johnali@microsoft.com>
2017-03-02 13:40:37 -0800

    Conversation bot

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ var builder = require('botbuilder');
 var connector = new builder.ConsoleConnector().listen();
 var bot  = new builder.UniversalBot(connector);
 
-bot.dialog('/',
-    (s) => { s.send("Hello world!"); debugger; }
-);
+bot.dialog('/', [
+    (s) => { builder.Prompts.text(s, "What's your name?"); },
+    (s,r) => { 
+        s.userData.name = r.response;
+        builder.Prompts.choice(s, "Choice?", "A|B|C");
+    },
+    (s,r) => {
+        s.send(r.response.entity);
+    }
+]);
